8b480bfa92188c40fa386ec11a6cee36f6d69367
R4aveen <rojaslillob@gmail.com>
2026-05-13 10:08:16 -0400

    feat:  mejora en la apariencia y responsividad del header y el canvas, efectos hover, y posicionamiento dinamico de los nav y offcanvas

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,3 +18,18 @@ document.addEventListener('DOMContentLoaded', function () {
         });
     }
 });
+
+ function updateOffcanvasSide() {
+        const offcanvas = document.getElementById('offcanvasExample');
+        if (!offcanvas) return;
+        if (window.innerWidth < 992) {
+            offcanvas.classList.remove('offcanvas-start');
+            offcanvas.classList.add('offcanvas-end');
+        } else {
+            offcanvas.classList.remove('offcanvas-end');
+            offcanvas.classList.add('offcanvas-start');
+        }
+    }
+    window.addEventListener('resize', updateOffcanvasSide);
+    window.addEventListener('DOMContentLoaded', updateOffcanvasSide);
+
