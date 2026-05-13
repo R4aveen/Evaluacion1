0e8bc443239a837c872b57ddf1df4959fb1215f7
r4veeen <101838253+R4aveen@users.noreply.github.com>
2026-05-13 18:14:43 -0400

    Feature/header style (#4)
    * feat: Agrega estilos bootstrap y estructura al header en index.html y custom.css
    
    * feat: implementar menu lateral offcanvas con iconos
    
    - Integrar componente offcanvas de Bootstrap para el menu principal.
    - Descargar e implementar íconos Feather locales para las opciones del menu
    - Añadir CSS custom para estilos hover y filtro de color de marca (.filter-orange)
    - Aislar logica en main.js para alternar el ícono del botón (menú a X) al abrir/cerrar.
    
    * feat: mejora el estilo del header y agrega funcionalidad al offcanvas
    
    * feat:  mejora en la apariencia y responsividad del header y el canvas, efectos hover, y posicionamiento dinamico de los nav y offcanvas
    
    * feat: se agrega al proyecto README y refinacion de responsividad en tablet y mobile
    
    * feat: actualizacion con la carpeta de icons, customizacion visual y crecion de root en custom.css
    
    * feat: implementacion de tipografia CSS de google fonts y root cusomizados con paleta de colores definida

---
+++ README.md
@@ -0,0 +1,41 @@
+# [Nombre del Proyecto]
+> Proyecto del Módulo 1 — HTML + CSS + JS · Diplomado Fullstack IPSS
+## Integrantes
+- [Bastian Rojas]
+- [Fransisco San Juan]
+## Descripción
+Proyecto del modulo 1, pagina web del banco Itaú Empresas (https://www.itau.cl/empresas) para empresas, se evalua el uso de HTML semantico, CSS y Bootstrap, ademas de JavaScript con minimo 2 addlisteners, ademas de al menos 1 evento personalizado y una configuracion de paleta root, en este proyecto tambien se evalua el manejo de interfaces responsivas (mobile first, desktop next) y el uso de git y github (https://github.com/R4aveen/Evaluacion1/).
+## Demo
+- Sitio desplegado: [URL de GitHub Pages si aplica]
+- Capturas:
+![Hero](docs/hero.png)
+## Cómo correr localmente
+
+```bash
+git clone [https://github.com/R4aveen/Evaluacion1.git]
+cd [Evaluacion1]
+# Abrir index.html en el navegador
+# O con un servidor:
+python3 -m http.server 8000
+```
+## Estructura del proyecto
+```
+.
+├── assets/
+│   ├── css/
+│   │   └── custom.css
+│   ├── js/
+│   │   └── main.js
+│   └── img/
+│       ├── logo-empresa-56x56.svg
+│       └── ...
+├── index.html
+├── .gitignore
+└── README.md
+```
+## Stack
+- HTML5 semántico
+- Bootstrap 5 (vía CDN)
+- CSS custom propio (paleta, tipografía, ajustes)
+- JavaScript vanilla
+Apéndice D — Component
---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const offcanvasElement = document.getElementById('offcanvasExample');
+    const btnMenu = document.getElementById('btn-menu-lateral');
+    const iconMenu = document.getElementById('icon-menu');
+    const iconClose = document.getElementById('icon-close');
+
+    if (offcanvasElement && btnMenu && iconMenu && iconClose) {
+        offcanvasElement.addEventListener('show.bs.offcanvas', function () {
+            btnMenu.classList.add('btn-menu-active');
+            iconMenu.classList.add('d-none');
+            iconClose.classList.remove('d-none');
+        });
+
+        offcanvasElement.addEventListener('hide.bs.offcanvas', function () {
+            btnMenu.classList.remove('btn-menu-active');
+            iconClose.classList.add('d-none');
+            iconMenu.classList.remove('d-none');
+        });
+    }
+});
+
+ function updateOffcanvasSide() {
+        const offcanvas = document.getElementById('offcanvasExample');
+        if (!offcanvas) return;
+        if (window.innerWidth < 991) {
+            offcanvas.classList.remove('offcanvas-start');
+            offcanvas.classList.add('offcanvas-end');
+        } else {
+            offcanvas.classList.remove('offcanvas-end');
+            offcanvas.classList.add('offcanvas-start');
+        }
+    }
+    window.addEventListener('resize', updateOffcanvasSide);
+    window.addEventListener('DOMContentLoaded', updateOffcanvasSide);
+
