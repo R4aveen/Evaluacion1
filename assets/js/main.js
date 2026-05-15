03e8eab35313e77613ac54552887248838f367c6
R4aveen <bas.rojasl@duocuc.cl>
2026-05-14 20:55:20 -0400

    feat(ui): agregar contenido principal, slider swiper y fixes responsivos
    - Maquetación de 5 nuevas secciones en el main.
    - Integración y funcionalidad de SwiperJS para los sliders (swiper.js).
    - Corrección del scroll horizontal y ajustes generales de responsividad.

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,20 +34,3 @@ document.addEventListener('DOMContentLoaded', function () {
     window.addEventListener('DOMContentLoaded', updateOffcanvasSide);
 
 
-const swiper = new Swiper('.swiper', {
-    loop: true,
-    effect: 'fade',
-    grabCursor: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
